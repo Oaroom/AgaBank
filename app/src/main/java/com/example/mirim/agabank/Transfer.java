5ba5ba728e0b1a2800e17afdc10044410e4f2f37
oaroom <unidevel2013@gmail.com>
2018-05-10 21:02:39 +0900

    layout_balanceまでかんりょう”

---
+++ app/src/main/java/com/example/mirim/agabank/Transfer.java
@@ -0,0 +1,23 @@
+package com.example.mirim.agabank;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Transfer extends AppCompatActivity {
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        Spinner bankSpinner = (Spinner) findViewById(R.id.bankSpinner);
+
+
+
+    }
+}
